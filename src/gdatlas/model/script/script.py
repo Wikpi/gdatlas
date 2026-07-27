b2d4447494e62987c31f457e3f645227f2e173f0
Wikpi <66695611+Wikpi@users.noreply.github.com>
2026-07-27 17:06:02 +0200

    Revert "feat: add model fetch method abstraction (#22)" (#23)
    This reverts commit f861add19efabbf03499e9ce58cab8fe28c9e230.

--- src/gdatlas/model/script/script.py
+++ src/gdatlas/model/script/script.py
@@ -1,14 +1,8 @@
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import TypeVar
 
 from gdatlas.model.script.elements import ScriptElement
 from gdatlas.model.script.metadata import ScriptMetadata
-
-ScriptElementType = TypeVar(
-    "ScriptElementType",
-    bound=ScriptElement,
-)
 
 
 @dataclass(slots=True)
@@ -17,20 +11,3 @@ class Script:
 
     metadata: list[ScriptMetadata] = field(default_factory=list, init=False)
     elements: list[ScriptElement] = field(default_factory=list, init=False)
-
-    def get_elements(self, target_type: type[ScriptElementType]) -> list[ScriptElementType]:
-        target_elements: list[ScriptElementType] = []
-
-        for element in self.elements:
-            if not isinstance(element, target_type):
-                continue
-            target_elements.append(element)
-
-        return target_elements
-
-    def get_metadata(self, target: str) -> ScriptMetadata | None:
-        for data in self.metadata:
-            if data.name != target:
-                continue
-            return data
-        return None
